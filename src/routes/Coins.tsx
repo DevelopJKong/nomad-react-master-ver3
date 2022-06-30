5b768d9de965d2b49d794a4b51f8bf75a0a3fa6e
DevelopJKong <ybaram40@gmail.com>
2022-06-30 22:47:02 +0900

    #4.3 Home part Two 완료

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,11 +1,89 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Title = styled.div`
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h3`
   color: ${(props) => props.theme.accentColor};
 `;
 
+const Loader = styled.div`
+  text-align: center;
+  display: block;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 function Coins() {
-  return <Title>Coins</Title>;
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const data = await (
+        await fetch("https://api.coinpaprika.com/v1/coins")
+      ).json();
+      setCoins(data.slice(0, 100));
+      setIsLoading(false);
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinsList>
+        <CoinsList>
+          {isLoading ? (
+            <Loader>Loading...</Loader>
+          ) : (
+            coins.map((coin) => (
+              <Coin key={coin.id}>
+                <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              </Coin>
+            ))
+          )}
+        </CoinsList>
+      </CoinsList>
+    </Container>
+  );
 }
 
 export default Coins;
